a65aec318f984c8331ffbbd1191a8a19ea705f57
TetyanaRed <redcars2012@gmail.com>
2021-10-29 14:14:20 +0300

    Кнопки Wait и Reset

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,3 @@
-export default function Button() {
-  return <button>Start</button>;
+export default function Button({ onClick, data }) {
+  return <button onClick={onClick}>{data}</button>;
 }
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,31 +1,60 @@
 import { useState, useEffect } from "react";
-// import Button from "../Button/Button";
+import Button from "../Button/Button";
 
 export default function Timer() {
   const [active, toggleActive] = useState(false);
   const [interval, changeInterval] = useState(0);
+  const [doubleClick, changedoubleClick] = useState([]);
 
   useEffect(() => {
     if (active) {
       window.termine = setInterval(() => {
         changeInterval((interval) => (interval += 1));
-      }, 10);
+      }, 1000);
     } else {
       clearInterval(window.termine);
     }
   }, [active]);
 
+  useEffect(() => {
+    if (doubleClick[1] - doubleClick[0] < 300) {
+      clearInterval(window.termine);
+      toggleActive((active) => !active);
+    }
+    if (doubleClick[1] - doubleClick[0] >= 300) {
+      console.log("ожидается двойной щелчек мыши");
+      changedoubleClick([]);
+    }
+
+    if (doubleClick.length === 2) changedoubleClick([]);
+  }, [doubleClick]);
+
+  const changeActive = () => {
+    toggleActive(!active);
+    if (active) changeInterval(0);
+  };
+
+  const waitClick = () => {
+    changedoubleClick((doubleClick) => [...doubleClick, new Date()]);
+  };
+
+  const reset = () => {
+    changeInterval(0);
+  };
+
   return (
     <>
       <div>
-        <div>{Math.floor(interval / 60 / 60)}</div>
-        <div>{Math.floor(interval / 60) % 60}</div>
-        <div>{interval % 60}</div>
+        <span>
+          {String(Math.floor(interval / 60 / 60)).padStart(2, 0)}:
+          {String(Math.floor(interval / 60) % 60).padStart(2, 0)}:
+          {String(interval % 60).padStart(2, 0)}
+        </span>
       </div>
-      <button onClick={() => toggleActive(!active)}>
-        {active ? "Stop" : "Start"}
-      </button>
-      {/* <Button /> */}
+
+      <Button onClick={changeActive} data={active ? "Stop" : "Start"} />
+      <Button onClick={waitClick} data={"Wait"} />
+      <Button onClick={reset} data={"Reset"} />
     </>
   );
 }
